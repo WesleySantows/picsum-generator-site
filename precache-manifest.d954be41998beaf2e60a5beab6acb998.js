9a99eb46ccc8dcfe30d0189cf991188d8346cf23
Wesley Santos <wesley@fuerzastudio.com.br>
2020-12-09 19:55:38 -0300

    Updates

---
+++ precache-manifest.d954be41998beaf2e60a5beab6acb998.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed575da93f04a9815321dc780426733f",
+    "url": "/picsum-generator-site/index.html"
+  },
+  {
+    "revision": "164cf08d76ac6309d883",
+    "url": "/picsum-generator-site/static/css/main.b9f7918b.chunk.css"
+  },
+  {
+    "revision": "8ed557406679669adb1e",
+    "url": "/picsum-generator-site/static/js/2.a7601f0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/picsum-generator-site/static/js/2.a7601f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "164cf08d76ac6309d883",
+    "url": "/picsum-generator-site/static/js/main.21c6ff63.chunk.js"
+  },
+  {
+    "revision": "6d10f64d1c518a5f37cc",
+    "url": "/picsum-generator-site/static/js/runtime-main.3252cf18.js"
+  }
+]);
